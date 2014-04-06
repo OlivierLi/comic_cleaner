2de587046904bf2dc1f7aa118aa7406c379c524a
Olivier Li <OlivierLiDupuis@gmail.com>
2014-04-05 20:06:35 -0400

    Add code to build a list of banned pages based on crc. Also light cleanup.

--- comicCleaner.py
+++ comicCleaner.py
@@ -5,6 +5,45 @@ import os
 import fnmatch
 import rarfile
 import zipfile
+import binascii
+
+#When a file with this crc is encountered it will be removed from the archive
+banned_crcs = []
+
+
+def crc32_from_file(filename):
+    buffer = open(filename, 'rb').read()
+    crc_value = binascii.crc32(buffer)
+    return crc_value
+
+
+def gather_crcs(directory):
+
+    if not os.path.isdir(directory) or not os.path.exists(directory):
+        print("The ad directory is invalid!")
+        sys.exit(-1)
+
+    crcs = []
+
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filneame in filenames:
+            full_path = os.path.abspath(os.path.join(dirpath, filneame))
+            crcs.append(crc32_from_file(full_path))
+
+    return crcs
+
+
+def clean_cbr(comic_path):
+
+    global banned_crcs
+
+    rf = rarfile.RarFile(comic_path)
+    page_info_list = rf.infolist()
+
+    for info in page_info_list:
+        for banned_crc in banned_crcs:
+            if info.CRC == banned_crc:
+                print("Banned page found in :" + comic_path)
 
 
 def is_cbr_valid(comic_path):
@@ -17,6 +56,8 @@ def is_cbr_valid(comic_path):
 
         if len(page_info_list) > 0:
             cover_size = page_info_list[0].file_size
+        else:
+            return False
 
         #See if each page is a single or double page based on the size
         for info in page_info_list:
@@ -48,6 +89,8 @@ def is_cbz_valid(comic_path):
 
         if len(page_info_list) > 0:
             cover_size = page_info_list[0].file_size
+        else:
+            return False
 
         #See if each page is a single or double page based on the size
         for info in page_info_list:
@@ -79,6 +122,10 @@ def main():
         print("You must provide a valid directory to look into!")
         sys.exit(-1)
 
+    #Lets find the crcs of the banned images
+    global banned_crcs
+    banned_crcs = gather_crcs("pages_to_remove")
+
     # First lets recursively find all the comics in the library 
     comics = []
     for root, dirnames, filenames in os.walk(directory):
@@ -95,6 +142,15 @@ def main():
         elif fnmatch.fnmatchcase(comic, "*.cbr"):
             if not is_cbr_valid(comic):
                 comics.remove(comic)
+
+    #Now that invalid archive are excluded we can proceed with cleaning them
+    for comic in comics:
+        if fnmatch.fnmatchcase(comic, "*.cbz"):
+            #TODO add cleaning function for cbzs
+            pass
+
+        elif fnmatch.fnmatchcase(comic, "*.cbr"):
+            clean_cbr(comic)
 
 
 if __name__ == "__main__":
